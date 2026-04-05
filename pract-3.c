66569974fbb37fc05c8b63fcd35766faa186322f
vishwas0229 <852006rahul@gmail.com>
2026-04-05 17:33:23 +0530

    pract-3 complete

--- pract-3.c
+++ pract-3.c
@@ -43,6 +43,88 @@ struct Node *insertAtIndex(struct Node *head, int data, int index)
     return head;
 }
 
+// Inserting at the end
+struct Node *insertAtEnd(struct Node *head, int data)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data = data;
+    struct Node *p = head;
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+    p->next = ptr;
+    ptr->next = NULL;
+    return head;
+}
+
+// Inserting after a given Node
+struct Node *insertAfterNode(struct Node *head, struct Node *prevNode, int data)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data = data;
+    ptr->next = prevNode->next;
+    prevNode->next = ptr;
+    return head;
+}
+
+// Deleting the first node
+struct Node *deleteAtFirst(struct Node *head)
+{
+    struct Node *ptr = head;
+    head = head->next;
+    free(ptr);
+    return head;
+}
+
+// Deleting the node at the index
+struct Node *deleteAtIndex(struct Node *head, int index)
+{
+    struct Node *p = head;
+    struct Node *q = head->next;
+    for (int i = 1; i < index - 1; i++)
+    {
+        p = p->next;
+        q = q->next;
+    }
+    p->next = q->next;
+    free(q);
+    return head;
+}
+
+// Deleting the last node
+struct Node *deleteAtEnd(struct Node *head)
+{
+    struct Node *ptr = head;
+    struct Node *q = head->next;
+    while (q->next != NULL)
+    {
+        ptr = ptr->next;
+        q = q->next;
+    }
+    ptr->next = NULL;
+    free(q);
+    return head;
+}
+
+// Deleting the first node with a given value
+struct Node *deleteByValue(struct Node *head, int value)
+{
+    struct Node *ptr = head;
+    struct Node *q = head->next;
+    while (q->data != value && q->next != NULL)
+    {
+        ptr = ptr->next;
+        q = q->next;
+    }
+    if (q->data == value)
+    {
+        ptr->next = q->next;
+        free(q);
+    }
+    return head;
+}
+
 int main()
 {
     int op, data, index;
@@ -69,8 +151,8 @@ int main()
     fourth->next = NULL;
 
     printf("Operation!!");
-    printf("\n1. Traversal\n2. Inserting at the beginning\n3. Inserting in between\n4. Inserting at the end\n5. Inserting after a given Node");
-    printf("\nChose a Operation(1 to 5): ");
+    printf("\n1. Traversal\n2. Inserting at the beginning\n3. Inserting in between\n4. Inserting at the end\n5. Inserting after a given Node\n6. Deleting the first node\n7. Deleting the node at the index\n8. Deleting the last node\n9. Deleting the first node with a given value");
+    printf("\nChose a Operation(1 to 9): ");
     scanf("%d", &op);
     switch (op)
     {
@@ -80,7 +162,10 @@ int main()
     case 2:
         printf("Enter element you want to insert: ");
         scanf("%d", &data);
+        printf("Linked list before insertion\n");
+        trevLinkedList(head);
         head = insertAtFirst(head, data);
+        printf("\nLinked list after insertion\n");
         trevLinkedList(head);
         break;
     case 3:
@@ -88,10 +173,95 @@ int main()
         scanf("%d", &data);
         printf("Enter index you want to insert: ");
         scanf("%d", &index);
+        printf("Linked list before insertion\n");
+        trevLinkedList(head);
         head = insertAtIndex(head, data, index);
+        printf("\nLinked list after insertion\n");
         trevLinkedList(head);
         break;
     case 4:
+        printf("Enter element you want to insert: ");
+        scanf("%d", &data);
+        printf("Linked list before insertion\n");
+        trevLinkedList(head);
+        head = insertAtEnd(head, data);
+        printf("\nLinked list after insertion\n");
+        trevLinkedList(head);
+        break;
+    case 5:
+        printf("Enter element you want to insert: ");
+        scanf("%d", &data);
+        printf("Enter node after you want to insert (1 to 4): ");
+        scanf("%d", &index);
+        switch (index)
+        {
+        case 1:
+            printf("Linked list before insertion\n");
+            trevLinkedList(head);
+            head = insertAfterNode(head, head, data);
+            printf("\nLinked list after insertion\n");
+            trevLinkedList(head);
+            break;
+        case 2:
+            printf("Linked list before insertion\n");
+            trevLinkedList(head);
+            head = insertAfterNode(head, second, data);
+            printf("\nLinked list after insertion\n");
+            trevLinkedList(head);
+            break;
+        case 3:
+            printf("Linked list before insertion\n");
+            trevLinkedList(head);
+            head = insertAfterNode(head, third, data);
+            printf("\nLinked list after insertion\n");
+            trevLinkedList(head);
+            break;
+        case 4:
+            printf("Linked list before insertion\n");
+            trevLinkedList(head);
+            head = insertAfterNode(head, fourth, data);
+            printf("\nLinked list after insertion\n");
+            trevLinkedList(head);
+            break;
+        default:
+            printf("Node not exist at this point.");
+            break;
+        }
+        break;
+    case 6:
+        printf("Linked list before deletion\n");
+        trevLinkedList(head);
+        head = deleteAtFirst(head);
+        printf("\nLinked list after deletion\n");
+        trevLinkedList(head);
+        break;
+    case 7:
+        printf("Enter index you want to delete: ");
+        scanf("%d", &index);
+        printf("Linked list before deletion\n");
+        trevLinkedList(head);
+        head = deleteAtIndex(head, index);
+        printf("\nLinked list after deletion\n");
+        trevLinkedList(head);
+        break;
+    case 8:
+        printf("Linked list before deletion\n");
+        trevLinkedList(head);
+        head = deleteAtEnd(head);
+        printf("\nLinked list after deletion\n");
+        trevLinkedList(head);
+        break;
+    case 9:
+        printf("Enter element you want to delete: ");
+        scanf("%d", &data);
+        printf("Linked list before deletion\n");
+        trevLinkedList(head);
+        head = deleteByValue(head, data);
+        printf("\nLinked list after deletion\n");
+        trevLinkedList(head);
+        break;
+    default:
+        printf("Chose correct operation!");
         break;
     }
     return 0;
